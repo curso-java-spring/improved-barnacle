3bd814593a8870eda35346eee99ed4343ce9dc79
Marco Mangan <marco.mangan@gmail.com>
2019-06-18 17:10:16 -0300

    #2

---
+++ src/main/java/hello/CustomerController.java
@@ -0,0 +1,20 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.*;
+
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/customers")
+    public String listAllCustomers(Model model) {
+		List<Customer> lista = new ArrayList<Customer>();
+		lista.add(new Customer("João", "Silva"));
+        model.addAttribute("cs", lista);
+        return "customersList";
+    }
+
+}
